e6e1e05791ab416a9785e74796d15e34f6108f95
LioTree <uc0110@163.com>
2024-06-10 16:33:38 +0100

    Change "Literal(Constant(todo))" to "to do"

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/TodoExtractor.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/TodoExtractor.scala
@@ -9,7 +9,7 @@ class TodoExtractor extends tpd.TreeTraverser {
   override def traverse(tree: tpd.Tree)(using ctx: Context): Unit = {
     tree match {
       case apply: tpd.Apply =>
-        if (apply.fun.toString.equals("Select(Select(Ident(sys),package),error)") && apply.args(0).toString.equals("Literal(Constant(todo))"))
+        if (apply.fun.toString.equals("Select(Select(Ident(sys),package),error)") && apply.args(0).toString.contains("todo))"))
           println (s"Found an apply: ${apply.fun} ${ctx.owner.showFullName}")
       case _ =>
     }
