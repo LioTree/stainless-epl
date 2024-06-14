986f39abe4e4b02dbbed58af567f908ec796d10b
LioTree <uc0110@163.com>
2024-06-14 21:53:09 +0100

    add benchmarks of exercise 11 in assignment 1

---
+++ frontends/benchmarks/cluster/assignment1/exercise11.scala
@@ -0,0 +1,19 @@
+object Test4 {
+  /* Exercise 11 */
+  def map[A, B](f: A => B, l: List[A]): List[B] =
+//    decreases(l)
+    l match {
+      case Nil => Nil
+      case x :: xs => f(x) :: map(f, xs)
+    }
+}
+
+object Test2 {
+  /* Exercise 11 */
+  def map[A, B](f: A => B, l: List[A]): List[B] =
+//    decreases(l)
+    l match {
+      case x :: xs => f(x) :: map(f, xs)
+      case Nil     => Nil
+    }
+}
