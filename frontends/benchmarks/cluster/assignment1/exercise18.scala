77e03c0f465eb0d44110f0240684f32ed1962d57
LioTree <uc0110@163.com>
2024-06-17 09:37:32 +0100

    add benchmarks of exercise 18 in assignment 1

--- frontends/benchmarks/cluster/assignment1/exercise18.scala
+++ frontends/benchmarks/cluster/assignment1/exercise18.scala
@@ -1,14 +1,12 @@
-// ListMap[Int,String]() --> ListMap.empty[Int,String]
-// Compared to converting, it might be more appropriate to add support for the stainless library.
 object Test4 {
-  // def map12_withUpdate = ListMap[Int, String]() + (1 ->"a") + (2 -> "b")
-  def map12_withUpdate = ListMap.empty[Int, String] + (1 -> "a") + (2 -> "b")
+   def map12_withUpdate = ListMap[Int, String]() + (1 ->"a") + (2 -> "b")
+//  def map12_withUpdate = ListMap.empty[Int, String] + (1 -> "a") + (2 -> "b")
 }
 
 object Test9 {
   def map12_withUpdate: ListMap[Int, String] = {
-//   val empty = ListMap[Int, String]();
-    val empty = ListMap.empty[Int, String];
+   val empty = ListMap[Int, String]();
+//    val empty = ListMap.empty[Int, String];
     empty.updated(1, "a").updated(2, "b")
   }
 }
