15061967ccf45383ef97f9c48e67e69dbfe42452
LioTree <uc0110@163.com>
2024-06-21 14:35:23 +0100

    Automatically expand cases with '|'

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/PureScalaTransform.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/PureScalaTransform.scala
@@ -42,14 +42,14 @@ class PureScalaTransform extends Phase {
             case name if name.isTypeName =>
               Ident(typeName("Int"))
           }
-//        case Ident(name) if name.toString == "Int" =>
-//          // Replace Int with BigInt.
-//          name match {
-//            case name if name.isTermName =>
-//              Ident(termName("BigInt"))
-//            case name if name.isTypeName =>
-//              Ident(typeName("BigInt"))
-//          }
+        //        case Ident(name) if name.toString == "Int" =>
+        //          // Replace Int with BigInt.
+        //          name match {
+        //            case name if name.isTermName =>
+        //              Ident(termName("BigInt"))
+        //            case name if name.isTypeName =>
+        //              Ident(typeName("BigInt"))
+        //          }
         case Ident(name) if name.toString == "Nil" =>
           // Replace Nil with Nil().
           Apply(Ident(termName("Nil")), Nil)
@@ -65,8 +65,8 @@ class PureScalaTransform extends Phase {
         case Select(qualifier, name) if name.toString == "abs" =>
           // Replace .abs with abs().
           Apply(Ident(termName("abs")), List(qualifier))
-//        case InfixOp(left, op: Ident, right) if op.name.toString == "^" =>
-//          Apply(Ident(termName("xor")), List(left, right))
+        //        case InfixOp(left, op: Ident, right) if op.name.toString == "^" =>
+        //          Apply(Ident(termName("xor")), List(left, right))
         case Apply(fun, args) if (fun.isInstanceOf[Ident] && fun.asInstanceOf[Ident].name.toString == "ListMap"
           || fun.isInstanceOf[Select] && fun.asInstanceOf[Select].toString.endsWith("ListMap)")) && args.nonEmpty =>
           args(0) match {
@@ -84,19 +84,19 @@ class PureScalaTransform extends Phase {
         case Apply(fun@Select(qualifier: Ident, name: TermName), args) if qualifier.name.toString == "math" =>
           // replace math.xx with xx because the stainless.math library is imported.
           Apply(Ident(name), super.transform(args))
-        case Apply(fun:Ident, args) if fun.name.toString == "println" =>
+        case Apply(fun: Ident, args) if fun.name.toString == "println" =>
           // replace println with print
           Apply(Ident(termName("print")), super.transform(args))
         case Throw(expr) =>
           // Replace throw with error[Nothing]("Error message.")
           // Although stainless supports the use of Exception(), its return type is not Nothing.
-//          Apply(Ident(termName("Exception")), List(Literal(Constants.Constant("Exception message."))))
+          //          Apply(Ident(termName("Exception")), List(Literal(Constants.Constant("Exception message."))))
           Apply(TypeApply(Ident(termName("error")), List(Ident(typeName("Nothing")))), List(Literal(Constants.Constant("Error message."))))
-//        case Number(_, _) =>
-           // Despite there is implicit conversion between BigInt and Int,
-           // there are still some cases where the conversion cannot be performed automatically (such as 1 -> "xxxx").
-           // Therefore, all Numbers are directly wrapped with BigInt()
-//          Apply(Ident(termName("BigInt")), List(tree))
+        //        case Number(_, _) =>
+        // Despite there is implicit conversion between BigInt and Int,
+        // there are still some cases where the conversion cannot be performed automatically (such as 1 -> "xxxx").
+        // Therefore, all Numbers are directly wrapped with BigInt()
+        //          Apply(Ident(termName("BigInt")), List(tree))
         case PackageDef(pid, stats) =>
           // Add `import stainless.collection._` `import stainless.annotation._` and
           // `import stainless.lang._` to the beginning of the file.
@@ -121,47 +121,57 @@ class PureScalaTransform extends Phase {
           // Remove all imports.
           EmptyTree
         case defDef@DefDef(name, paramss, tpt, _) =>
-            val defDefDetector = new DefDefDetector(defDef)
-            if(defDefDetector.unSupported || name.toString == "nameFromNum") {
-              val externIdent = Ident(typeName("extern"))
-              // Translation to English: A very necessary step, otherwise errors will occur in the typer.
-              // It took two out of three days to find the problem...
-              externIdent.span = Span(defDef.span.start, defDef.span.start + 7)
-              val externAnnotation = Apply(Select(New(externIdent), termName("<init>")), Nil)
+          val defDefDetector = new DefDefDetector(defDef)
+          if (defDefDetector.unSupported || name.toString == "nameFromNum") {
+            val externIdent = Ident(typeName("extern"))
+            // Translation to English: A very necessary step, otherwise errors will occur in the typer.
+            // It took two out of three days to find the problem...
+            externIdent.span = Span(defDef.span.start, defDef.span.start + 7)
+            val externAnnotation = Apply(Select(New(externIdent), termName("<init>")), Nil)
 
-              val pureIdent = Ident(typeName("pure"))
-              pureIdent.span = Span(defDef.span.start + 8, defDef.span.start + 8 + 5)
-              val pureAnnotation = Apply(Select(New(pureIdent), termName("<init>")), Nil)
+            val pureIdent = Ident(typeName("pure"))
+            pureIdent.span = Span(defDef.span.start + 8, defDef.span.start + 8 + 5)
+            val pureAnnotation = Apply(Select(New(pureIdent), termName("<init>")), Nil)
 
-              cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(defDef.rhs)).withAnnotations(List(externAnnotation, pureAnnotation))
-            }
-            else {
-              // Add decreases annotation automatically.
-              if (!defDefDetector.decreases.isEmpty) {
-                val decreasesApplies: ArrayBuffer[Apply] = ArrayBuffer.empty
-                defDefDetector.decreases.foreach(decrease =>
-                  decreasesApplies += Apply(Ident(termName("decreases")), List(decrease))
-                )
-                val newRhs = defDef.rhs match {
-                  case Block(stats, expr) =>
-                    Block(
-                      decreasesApplies.toList ::: stats,
-                      expr
-                    )
-                  case _ =>
-                    // The function body originally only had one statement. Wrap it in a block.
-                    Block(
-                      decreasesApplies.toList,
-                      defDef.rhs
-                    )
-                }
-                // remove all original annotations
-                cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(newRhs)).withAnnotations(Nil)
+            cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(defDef.rhs)).withAnnotations(List(externAnnotation, pureAnnotation))
+          }
+          else {
+            // Add decreases annotation automatically.
+            if (!defDefDetector.decreases.isEmpty) {
+              val decreasesApplies: ArrayBuffer[Apply] = ArrayBuffer.empty
+              defDefDetector.decreases.foreach(decrease =>
+                decreasesApplies += Apply(Ident(termName("decreases")), List(decrease))
+              )
+              val newRhs = defDef.rhs match {
+                case Block(stats, expr) =>
+                  Block(
+                    decreasesApplies.toList ::: stats,
+                    expr
+                  )
+                case _ =>
+                  // The function body originally only had one statement. Wrap it in a block.
+                  Block(
+                    decreasesApplies.toList,
+                    defDef.rhs
+                  )
               }
-              else
-                // remove all original annotations
-                cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(defDef.rhs)).withAnnotations(Nil)
+              // remove all original annotations
+              cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(newRhs)).withAnnotations(Nil)
             }
+            else
+              // remove all original annotations
+              cpy.DefDef(defDef)(name, transformParamss(paramss), transform(tpt), transform(defDef.rhs)).withAnnotations(Nil)
+          }
+        case Match(selector, cases) =>
+          // Find whether there is Alternative in cases
+          val flatCases = cases.flatMap(case_ =>
+            case_.pat match {
+              case Alternative(elements) =>
+                elements.collect{ case element => CaseDef(element, case_.guard, case_.body) }
+              case _ =>
+                List(case_)
+            })
+          cpy.Match(tree)(super.transform(selector), super.transformSub(flatCases))
         case _ =>
           super.transform(tree)
       }
