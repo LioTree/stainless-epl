23c6b146f2d81753439e805e334fd122507e2060
LioTree <uc0110@163.com>
2024-06-22 15:23:52 +0100

    add comments and remove useless code

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/PureScalaTransform.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/PureScalaTransform.scala
@@ -14,59 +14,68 @@ import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Stack
 import scala.collection.mutable.Set
 
-// Similar to `MacroTransform`, but we need to handle `untpdTree`,
-// so we cannot directly inherit from it.
+/**
+ * This class is a custom transformer for Scala code. It is similar to `MacroTransform`,
+ * but it handles `untpdTree`, so it cannot directly inherit from `MacroTransform`.
+ * It performs transformations on the Scala code to make it compatible with the Stainless verification tool.
+ */
 class PureScalaTransform extends Phase {
 
   import ast.untpd._
 
+  // The name of this phase in the compiler pipeline
   override val phaseName = "pure Scala Transform"
 
+  /**
+   * The main method of this phase, which applies the transformation to the compilation unit.
+   */
   override def run(using Context): Unit = {
     val unit = ctx.compilationUnit
     unit.untpdTree = atPhase(transformPhase)(newTransformer.transform(unit.untpdTree))
   }
 
+  // Creates a new instance of the transformer
   private def newTransformer(using Context): PureScalaTransformer = new PureScalaTransformer
 
+  // Returns the current phase
   private def transformPhase(using Context): Phase = this
 
+  /**
+   * This class performs the actual transformations on the Scala code.
+   * It extends `UntypedTreeMap`, which is a class for transforming untyped trees.
+   */
   private class PureScalaTransformer extends UntypedTreeMap {
+    /**
+     * The main method of this class, which performs the transformations on the given tree.
+     * It matches on the structure of the tree and applies the appropriate transformation.
+     */
     override def transform(tree: Tree)(using Context): Tree = {
       tree match {
+        // Replace Double with Int.
+        // Translating to Real might be a better choice, but it involves type conversion between Int and Real, which will be considered later.
         case Ident(name) if name.toString == "Double" =>
           name match {
             case name if name.isTermName =>
-              // Replace Double with Int.
               Ident(termName("Int"))
             case name if name.isTypeName =>
               Ident(typeName("Int"))
           }
-        //        case Ident(name) if name.toString == "Int" =>
-        //          // Replace Int with BigInt.
-        //          name match {
-        //            case name if name.isTermName =>
-        //              Ident(termName("BigInt"))
-        //            case name if name.isTypeName =>
-        //              Ident(typeName("BigInt"))
-        //          }
+        // Replace Nil with Nil().
         case Ident(name) if name.toString == "Nil" =>
-          // Replace Nil with Nil().
           Apply(Ident(termName("Nil")), Nil)
+        // Replace None with None().
         case Ident(name) if name.toString == "None" =>
-          // Replace None with None().
           Apply(Ident(termName("None")), Nil)
+        // Replace scala.collection.immutable.ListMap with ListMap.
         case Select(Select(Select(Ident(name1), name2), name3), name4) if s"$name1.$name2.$name3.$name4" == "scala.collection.immutable.ListMap" =>
-          // Replace scala.collection.immutable.ListMap with ListMap.
           name4 match {
             case name if name.isTermName => Ident(termName("ListMap"))
             case name if name.isTypeName => Ident(typeName("ListMap"))
           }
+        // Replace .abs with abs().
         case Select(qualifier, name) if name.toString == "abs" =>
-          // Replace .abs with abs().
           Apply(Ident(termName("abs")), List(qualifier))
-        //        case InfixOp(left, op: Ident, right) if op.name.toString == "^" =>
-        //          Apply(Ident(termName("xor")), List(left, right))
+        // Handling ListMap initialization.
         case Apply(fun, args) if (fun.isInstanceOf[Ident] && fun.asInstanceOf[Ident].name.toString == "ListMap"
           || fun.isInstanceOf[Select] && fun.asInstanceOf[Select].toString.endsWith("ListMap)")) && args.nonEmpty =>
           args(0) match {
@@ -78,28 +87,21 @@ class PureScalaTransform extends Phase {
               // Adding direct support for initializing ListMap with multiple ArrowAssoc since stainless doesn't support SeqLiteral.
               Apply(transform(fun), List(Apply(Ident(termName("List")), transform(args))))
           }
+        // replace sys.error() with error[Nothing]("Error message.")
         case Apply(fun@Select(qualifier: Ident, name: TermName), args) if qualifier.name.toString == "sys" && name.toString == "error" =>
-          // replace sys.error() with error[Nothing]("Error message.")
           Apply(TypeApply(Ident(termName("error")), List(Ident(typeName("Nothing")))), List(Literal(Constants.Constant("Error message."))))
+        // replace math.xx with xx because the stainless.math library is imported.
         case Apply(fun@Select(qualifier: Ident, name: TermName), args) if qualifier.name.toString == "math" =>
-          // replace math.xx with xx because the stainless.math library is imported.
           Apply(Ident(name), super.transform(args))
+        // replace println with print
         case Apply(fun: Ident, args) if fun.name.toString == "println" =>
-          // replace println with print
           Apply(Ident(termName("print")), super.transform(args))
+        // Replace throw with error[Nothing]("Error message.")
         case Throw(expr) =>
-          // Replace throw with error[Nothing]("Error message.")
-          // Although stainless supports the use of Exception(), its return type is not Nothing.
-          //          Apply(Ident(termName("Exception")), List(Literal(Constants.Constant("Exception message."))))
+          // Although stainless supports the use of Exception(), its return type is not Nothing. Therefore, we use error[Nothing] instead.
           Apply(TypeApply(Ident(termName("error")), List(Ident(typeName("Nothing")))), List(Literal(Constants.Constant("Error message."))))
-        //        case Number(_, _) =>
-        // Despite there is implicit conversion between BigInt and Int,
-        // there are still some cases where the conversion cannot be performed automatically (such as 1 -> "xxxx").
-        // Therefore, all Numbers are directly wrapped with BigInt()
-        //          Apply(Ident(termName("BigInt")), List(tree))
+        // Add `import stainless.collection._` `import stainless.annotation._` and `import stainless.lang._` to the beginning of the file.
         case PackageDef(pid, stats) =>
-          // Add `import stainless.collection._` `import stainless.annotation._` and
-          // `import stainless.lang._` to the beginning of the file.
           val importCollection = Import(
             Select(Ident(termName("stainless")), termName("collection")),
             List(ImportSelector(Ident(termName("_")), EmptyTree, EmptyTree))
@@ -117,14 +119,14 @@ class PureScalaTransform extends Phase {
             List(ImportSelector(Ident(termName("_")), EmptyTree, EmptyTree))
           )
           cpy.PackageDef(tree)(transformSub(pid), importCollection :: importAnnotation :: importLang :: importMath :: transformStats(stats, ctx.owner))
+        // Remove all original imports.
         case Import(expr, selectors) =>
-          // Remove all imports.
           EmptyTree
         case defDef@DefDef(name, paramss, tpt, _) =>
           val defDefDetector = new DefDefDetector(defDef)
           if (defDefDetector.unSupported || name.toString == "nameFromNum") {
             val externIdent = Ident(typeName("extern"))
-            // Translation to English: A very necessary step, otherwise errors will occur in the typer.
+            // A very necessary step, otherwise errors will occur in the typer.
             // It took two out of three days to find the problem...
             externIdent.span = Span(defDef.span.start, defDef.span.start + 7)
             val externAnnotation = Apply(Select(New(externIdent), termName("<init>")), Nil)
@@ -167,7 +169,7 @@ class PureScalaTransform extends Phase {
           val flatCases = cases.flatMap(case_ =>
             case_.pat match {
               case Alternative(elements) =>
-                elements.collect{ case element => CaseDef(element, case_.guard, case_.body) }
+                elements.collect { case element => CaseDef(element, case_.guard, case_.body) }
               case _ =>
                 List(case_)
             })
@@ -177,6 +179,10 @@ class PureScalaTransform extends Phase {
       }
     }
 
+    /**
+     * This class is used to detect certain patterns in the definition of a function.
+     * It traverses the tree of the function and collects information about it.
+     */
     private class DefDefDetector(defDef: DefDef)(using Context) extends UntypedTreeTraverser {
       private val matches: Stack[Ident | Boolean] = Stack.empty
       private val cases: Stack[Ident] = Stack.empty
@@ -202,6 +208,10 @@ class PureScalaTransform extends Phase {
         }
       }
 
+      /**
+       * This method traverses the tree and collects information about it.
+       * It detects certain patterns in the tree and updates the state of the detector accordingly.
+       */
       override def traverse(tree: untpd.Tree)(using Context): Unit = {
         tree match {
           case CaseDef(pat, guard, body) =>
