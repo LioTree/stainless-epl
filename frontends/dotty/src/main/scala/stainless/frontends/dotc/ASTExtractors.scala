4e9b2e22ed11b6508fe80f7ccc1bcffac8fb0f0a
LioTree <uc0110@163.com>
2024-07-19 14:10:18 +0100

    fix bug of bigSubstring in ASTExtractors

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/ASTExtractors.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/ASTExtractors.scala
@@ -1277,7 +1277,7 @@ trait ASTExtractors {
     object ExBigSubstringExpression {
       def unapply(tree: tpd.Apply) : Option[(tpd.Tree, tpd.Tree)] = tree match {
         case Apply(Select(
-          Apply(ExSelected("stainless", "lang", "package$", "StringDecorations"), stringExpr :: Nil),
+          Apply(ExSymbol("stainless", "lang", "package$", "StringDecorations"), stringExpr :: Nil),
           ExNamed("bigSubstring")), startExpr :: Nil)
           => Some(stringExpr, startExpr)
         case _ => None
@@ -1288,7 +1288,7 @@ trait ASTExtractors {
     object ExBigSubstring2Expression {
       def unapply(tree: tpd.Apply) : Option[(tpd.Tree, tpd.Tree, tpd.Tree)] = tree match {
         case Apply(Select(
-          Apply(ExSelected("stainless", "lang", "package$", "StringDecorations"), stringExpr :: Nil),
+          Apply(ExSymbol("stainless", "lang", "package$", "StringDecorations"), stringExpr :: Nil),
           ExNamed("bigSubstring")), startExpr :: endExpr :: Nil)
           => Some(stringExpr, startExpr, endExpr)
         case _ => None
