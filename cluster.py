23bbdbe5beba3fd1929b8567bc242fdccc80a2f0
LioTree <uc0110@163.com>
2024-07-22 22:34:55 +0100

    update cluster.py

--- cluster.py
+++ cluster.py
@@ -6,18 +6,36 @@ import os
 def parse_args():
     parser = argparse.ArgumentParser(description="Run stainless-dotty with various parameters.")
     parser.add_argument('--filenames', type=str, required=True, help='Comma-separated list of filenames')
-    parser.add_argument('--assn2', action='store_true', help='Boolean flag for assn2')
+    parser.add_argument('--assn1', type=bool, default=False, help='Boolean flag for assn1')
+    parser.add_argument('--assn2', type=bool, default=False, help='Boolean flag for assn2')
     parser.add_argument('--externpure', type=str, help='Comma-separated list of externpure')
     parser.add_argument('--extract', type=str, required=True, help='Comma-separated list of extract')
-    parser.add_argument('--pubclasses', type=str, help='Comma-separated list of pubclasses')
-    parser.add_argument('--pubclasses-pn', type=str, help='String for pubclasses-pn')
-    parser.add_argument('--comparefuns', type=str, required=True, help='Comma-separated list of comparefuns')
-    parser.add_argument('--models', type=str, required=True, help='Comma-separated list of models')
+    parser.add_argument('--pubdefs', type=str, help='Comma-separated list of pubdefs')
     parser.add_argument('--output', type=str, default='result.json', help='Output JSON file name')
+    parser.add_argument('--debug', type=bool, default=False, help='Show debug messages of transformation')
     return parser.parse_args()
 
+def generate_comparefuns_models(filenames, extract):
+    comparefuns = set()
+    models = set()
+
+    # Extract the first element from filenames for models
+    if filenames:
+        filename = filenames[0].split('/')[-1].replace('.scala', '')
+        packagename = filename.replace('-', '_')
+        models.add(packagename + '.' + packagename + '$package.' + extract)
+
+    # Extract the remaining elements from filenames for comparefuns
+    if len(filenames) > 1:
+        comparefuns.update(
+            f"{filename.replace('.scala', '').split('/')[-1].replace('-','_')}.{filename.replace('.scala', '').split('/')[-1]}$package.{extract}"
+            for filename in filenames[1:])
+
+    return comparefuns, models
+
+
 def run_dotty(filenames, params):
-    command = ['./frontends/dotty/target/universal/stage/bin/stainless-dotty']
+    command = ['/home/liontree/source/scala/stainless-plus/frontends/dotty/target/universal/stage/bin/stainless-dotty']
     command.extend(filenames.split(','))
     for key, value in params.items():
         if value:
@@ -30,73 +48,69 @@ def run_dotty(filenames, params):
     command.append('--timeout=3')
     command.append('--equivchk-output=temp.json')
 
+    print("[*] Running command:", ' '.join(command))
     subprocess.run(command)
 
-def read_temp_json():
-    with open('temp.json', 'r') as f:
+def read_json(filename):
+    with open(filename, 'r') as f:
         return json.load(f)
 
-def extract_function_name(full_name):
-    """Remove the part of the function name before the first dot."""
-    return full_name.split('.', 1)[1] if '.' in full_name else full_name
-
-def update_comparefuns(comparefuns, temp_data):
-    comparefuns = set(comparefuns.split(','))
+def update_comparefuns(comparefuns, models, temp_data):
+    models.clear()
     for item in temp_data.get('unsafe', []):
-        comparefuns.discard(extract_function_name(item['function']))
+        comparefuns.discard(item['function'])
+    for item in temp_data.get('unknownSafety', []):
+        comparefuns.discard(item['function'])
+    for item in temp_data.get('wrong', []):
+        comparefuns.discard(item)
     for item in temp_data.get('equivalent', []):
-        functions = [extract_function_name(func) for func in item['functions']]
+        functions = [func for func in item['functions']]
         comparefuns.difference_update(functions)
-    unequivalent_functions = [extract_function_name(item['function']) for item in temp_data.get('unequivalent', [])]
-    unknown_equivalence_functions = [extract_function_name(func) for func in temp_data.get('unknownEquivalence', [])]
-    unequivalent_functions.extend(unknown_equivalence_functions)
+    unequivalent_functions = [item['function'] for item in temp_data.get('unequivalent', [])]
+    unknown_functions = [func for func in temp_data.get('unknownEquivalence', [])]
+    unequivalent_functions.extend(unknown_functions)
+    print("[*] unequivalent or unknown functions:", unequivalent_functions)
     if unequivalent_functions:
-        first_unequivalent = unequivalent_functions[0]
-        comparefuns.discard(first_unequivalent)
-        return list(comparefuns), first_unequivalent
-    return list(comparefuns), None
+        models.add(unequivalent_functions[0])
+        comparefuns.discard(unequivalent_functions[0])
+
 
 def main():
     args = parse_args()
-    
-    filenames = args.filenames
-    extract = args.extract
-    comparefuns = args.comparefuns
-    models = args.models
-
-    required_params = {
-        'extract': extract,
-        'comparefuns': comparefuns,
-        'models': models,
-    }
-
-    optional_params = {
-        'assn2': args.assn2,
-        'externpure': args.externpure,
-        'pubclasses': args.pubclasses,
-        'pubclasses-pn': args.pubclasses_pn,
-    }
+    comparefuns, models = generate_comparefuns_models(args.filenames.split(','), args.extract.split(',')[0])
+    print("[*] comparefuns:", comparefuns)
+    print("[*] models:", models) 
 
     temp_json_results = []
-    comparefuns_list = comparefuns.split(',')
-    models_list = models.split(',')
 
-    while comparefuns_list:
-        models_list = []
-        run_dotty(filenames, {**required_params, **optional_params})
-        temp_data = read_temp_json()
+    while comparefuns:
+        params = {
+        'extract': args.extract,
+        'comparefuns': ','.join(comparefuns),
+        'models': ','.join(models),
+        'assn1': args.assn1,
+        'assn2': args.assn2,
+        'externpure': args.externpure,
+        'pubdefs': args.pubdefs,
+        }
+        if(args.debug):
+            params['debug'] = "transformation"
+
+        run_dotty(args.filenames, params)
+        temp_data = read_json('temp.json')
+        if(len(temp_data['equivalent']) == 0):
+            temp_data['equivalent'].append({'model': list(models), 'functions': []})
+        else:
+            temp_data['equivalent'][0]['model'] = list(models)
         temp_json_results.append(temp_data)
 
-        comparefuns_list, first_unequivalent = update_comparefuns(','.join(comparefuns_list), temp_data)
+        update_comparefuns(comparefuns, models, temp_data)
 
-        if first_unequivalent:
-            models_list.append(first_unequivalent)
+        print("[*] new comparefuns:", comparefuns)
+        print("[*] new models:", models)
 
-        required_params['comparefuns'] = ','.join(comparefuns_list)
-        required_params['models'] = ','.join(models_list)
-
-        print("[*] new comparefuns:", comparefuns_list)
-        print("[*] new models:", models_list)
+    if models:
+        temp_json_results.append({'equivalent': [{"models": list(models), "functions": []}]})
 
     with open(args.output, 'w') as f:
         json.dump(temp_json_results, f, indent=4)
