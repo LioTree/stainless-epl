8021cd7077b948ec570d08acab736d99c476be8a
LioTree <uc0110@163.com>
2024-08-03 21:04:02 +0100

    generate FakeValue class in Assn2

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/epl/Assn2Processor.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/epl/Assn2Processor.scala
@@ -29,7 +29,7 @@ class Assn2Processor(using dottyCtx: DottyContext, inoxCtx: inox.Context) extend
       override def transform(tree: Tree)(using DottyContext): Tree =
         tree match {
           case Apply(Ident(name), args) if name.toString == baseFun.name.toString =>
-            cpy.Apply(tree)(Ident(termName(fakePrefix + name.toString)), transform(args))
+            cpy.Apply(tree)(termIdent(fakePrefix + name.toString), transform(args))
           case _ => super.transform(tree)
         }
     }
@@ -42,14 +42,14 @@ class Assn2Processor(using dottyCtx: DottyContext, inoxCtx: inox.Context) extend
     def getSubFuns(using dottyCtx: DottyContext): List[DefDef] = {
       traverse(baseFun)
       // try to generate the default sub-function if it is not generated
-      genDefaultSubFn(TypeApply(Ident(termName("errorWrapper")), List(Ident(typeName("Nothing")))))
+      genDefaultSubFn(errorWrapper)
       subFuns
     }
 
     private def markSubFun(subFun: DefDef, markName: String): DefDef = {
       val spanStart = subFun.span.start
 
-      val subFnIdent = Ident(typeName("subFn"))
+      val subFnIdent = typeIdent("subFn")
       // A very necessary step, otherwise errors will occur in the typer.
       subFnIdent.span = Span(spanStart, spanStart + 6)
       val fileName = extractFileName(dottyCtx.source.toString)
@@ -64,7 +64,7 @@ class Assn2Processor(using dottyCtx: DottyContext, inoxCtx: inox.Context) extend
       def markDefaultSubFun(subFun: DefDef, markName: String): DefDef = {
         val spanStart = subFun.span.start
 
-        val subFnIdent = Ident(typeName("subFn"))
+        val subFnIdent = typeIdent("subFn")
         // A very necessary step, otherwise errors will occur in the typer.
         subFnIdent.span = Span(spanStart, spanStart + 6)
         val fileName = extractFileName(dottyCtx.source.toString)
@@ -72,7 +72,7 @@ class Assn2Processor(using dottyCtx: DottyContext, inoxCtx: inox.Context) extend
           List(Literal(Constants.Constant(s"${fileName}.${fileName}$$package.${baseFun.name.toString}")),
             Literal(Constants.Constant(markName))))
 
-        val defaultSubFnIdent = Ident(typeName("defaultSubFn"))
+        val defaultSubFnIdent = typeIdent("defaultSubFn")
         defaultSubFnIdent.span = Span(spanStart, spanStart + 14)
         val defaultSubFnAnnotation = Apply(Select(New(defaultSubFnIdent), termName("<init>")), Nil)
 
@@ -118,53 +118,99 @@ class Assn2Processor(using dottyCtx: DottyContext, inoxCtx: inox.Context) extend
       }
   }
 
-  def genFakeFun(baseFun: DefDef): DefDef = {
-    val newName = termName(fakePrefix + baseFun.name.toString)
-    val newRes = TypeApply(Ident(termName("errorWrapper")), List(Ident(typeName("Nothing"))))
-    val fakeFun = cpy.DefDef(baseFun)(newName, baseFun.paramss, baseFun.tpt, newRes)
-    markExternPure(fakeFun).asInstanceOf[DefDef]
-  }
-
   override def start(tree: untpd.Tree)(using DottyContext): untpd.Tree =
     inoxCtx.options.findOption(optAssn2) match {
       case Some(true) => transform(tree)
       case _ => tree
     }
 
+
+  private def genFakeFun(baseFun: DefDef): DefDef = {
+    val newName = termName(fakePrefix + baseFun.name.toString)
+    val newRes = errorWrapper
+    val fakeFun = cpy.DefDef(baseFun)(newName, baseFun.paramss, baseFun.tpt, newRes)
+    markExternPure(fakeFun).asInstanceOf[DefDef]
+  }
+
+  private def fakeValue: TypeDef =
+    TypeDef(typeName("FakeValue"), Template(DefDef(termName("<init>"), Nil, TypeTree(), EmptyTree), Nil, EmptyValDef, Nil))
+
+//  private def value2FakeValue(spanStart: Int): DefDef = {
+//    val paramType = typeIdent("Value")
+//    paramType.span = Span(spanStart, spanStart + 5)
+//
+//    val newValDef = ValDef(termName("s"), para, EmptyTree)
+//    newValDef.span = Span(spanStart, spanStart + 5)
+//
+//    val returnType = typeIdent("FakeValue")
+//    returnType.span = Span(spanStart + 6, spanStart + 10)
+//
+//    val newDefDef = DefDef(termName("value2FakeValue"),
+//      List(List(newValDef)),
+//      returnType, errorWrapper)
+//    newDefDef.span = Span(spanStart + 11, spanStart + 20)
+//    newDefDef.withFlags(Flags.Implicit)
+//    newDefDef.mods.withAddedMod(Mod.Implicit())
+//    markExternPure(newDefDef).asInstanceOf[DefDef]
+//  }
+
+//  private def fakeValue2Value(spanStart: Int): DefDef = {
+//    val newValDef = ValDef(termName("f"), typeIdent("FakeValue"), EmptyTree)
+//    newValDef.span = Span(spanStart, spanStart + 5)
+//
+//    val newIdent = typeIdent("Value")
+//    newIdent.span = Span(spanStart + 6, spanStart + 10)
+//
+//    val newDefDef = DefDef(termName("fakeValue2Value"),
+//      List(List(newValDef)),
+//      newIdent, errorWrapper)
+//    newDefDef.span = Span(spanStart + 11, spanStart + 20)
+//    newDefDef.withFlags(Flags.Implicit)
+//    newDefDef.mods.withAddedMod(Mod.Implicit())
+//    markExternPure(newDefDef).asInstanceOf[DefDef]
+//  }
+
   override def transform(tree: untpd.Tree)(using DottyContext): untpd.Tree =
     tree match {
       case PackageDef(pid: Ident, stats) if pid.name.toString == "<empty>" => {
         var subFunctions = List.empty[DefDef]
-        var fakeFunctions = List.empty[DefDef]
+//        var fakeDefs: List[Tree] = List(fakeValue, value2FakeValue(pid.span.end), fakeValue2Value(pid.span.end + 21))
+//        var fakeDefs: List[Tree] = List(fakeValue, value2FakeValue(pid.span.end))
+        var fakeDefs: List[Tree] = List(fakeValue)
 
         val newStats = stats.flatMap {
           // Replace Map with ListMap to avoid https://github.com/epfl-lara/stainless/issues/1547
-          case typeDef@TypeDef(name, rhs@LambdaTypeTree(tparams, body: AppliedTypeTree)) if name.toString == "Env" =>
-            val newBody = cpy.AppliedTypeTree(body)(Ident(typeName("ListMap")), transform(body.args))
-            List(cpy.TypeDef(typeDef)(name, cpy.LambdaTypeTree(rhs)(transformSub(tparams), transform(newBody))))
+//          case typeDef@TypeDef(name, rhs@LambdaTypeTree(tparams, body: AppliedTypeTree)) if name.toString == "Env" =>
+//            val newBody = cpy.AppliedTypeTree(body)(Ident(typeName("ListMap")), transform(body.args))
+//            List(cpy.TypeDef(typeDef)(name, cpy.LambdaTypeTree(rhs)(transformSub(tparams), transform(newBody))))
 
           case moduleDef@ModuleDef(name, impl) if name.toString == "Gensym" =>
             List(markExternPure(untpd.cpy.ModuleDef(moduleDef)(name, transformSub(impl))))
 
           case defDef@DefDef(name, paramss, tpt, _) if splitFunctions.contains(name.toString) =>
             subFunctions = subFunctions ++ (new SubFunctionGenerator(defDef)).getSubFuns
-            fakeFunctions = fakeFunctions :+ genFakeFun(defDef)
+            fakeDefs = fakeDefs :+ genFakeFun(defDef)
             List(transform(defDef))
 
           case other => List(transform(other))
-        } ++ fakeFunctions ++ subFunctions
+        } ++ fakeDefs ++ subFunctions
 
         super.transform(cpy.PackageDef(tree)(pid, newStats))
       }
 
       case Apply(Ident(name), args) if safeListMap.contains(name.toString) =>
-        Apply(Select(Ident(name), termName("getOrElse")), args :+ TypeApply(Ident(termName("errorWrapper")), List(Ident(typeName("Nothing")))))
+        Apply(Select(Ident(name), termName("getOrElse")), args :+ errorWrapper)
 
       case Apply(Select(Ident(name),name2), args) if (name2.toString == "apply" || name2.toString == "get") && safeListMap.contains(name.toString) =>
-        Apply(Select(Ident(name), termName("getOrElse")), args :+ TypeApply(Ident(termName("errorWrapper")), List(Ident(typeName("Nothing")))))
+        Apply(Select(Ident(name), termName("getOrElse")), args :+ errorWrapper)
 
-      case Apply(Select(qualifier, name), args) if name.toString == "equals" || name.toString == "eq" =>
-        InfixOp(transform(qualifier), Ident(termName("==")), transform(args(0)))
+        // Not sure whether we should put this into pure scala translator.
+        // Stainless does not support custom == equals, which may cause semantic inconsistencies.
+      case Apply(Select(qualifier, name), List(arg)) if name.toString == "equals" || name.toString == "eq" =>
+        InfixOp(transform(qualifier), termIdent("=="), transform(arg))
+
+      case untpd.AppliedTypeTree(tpt: Ident, List(arg:Ident)) if tpt.name.toString == "Env" && arg.name.toString == "Value" =>
+        cpy.AppliedTypeTree(tree)(tpt, List(typeIdent("FakeValue")))
 
       case _ => super.transform(tree)
     }
